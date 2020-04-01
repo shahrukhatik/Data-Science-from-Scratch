fecfc60a09074ab363b1372a3dc8213950215fb4
ShahrukhAtik <shahrukhatik@Gmail.com>
2020-04-01 17:32:41 -0500

    Introduction, Organizing 1.01: Updating Code, Structuring 1.02: Page 5 Complete

--- Chapter 1 - Intro/Introduction.py
+++ Chapter 1 - Intro/Introduction.py
@@ -15,4 +15,20 @@ users = [
 
 friendship_pairs = [(0,1),(0,2),(1,2),(1,3),(2,3),(3,4),(4,5),(5,6),(5,7),(6,8),(7,8),(8,9)]
 
+## Initialize the dict with an empty list for each user id:
+friendships = {user["id"]: [] for user in users}
 
+#Loop over friendship pairs to populate it
+for i, j in friendship_pairs:
+    friendships[i].append(j) ## add j as a friend of user i
+    friendships[j].append(i) ## add i as a friend of user j
+
+def number_of_friends(user):
+    user_id = user["id"]
+    friend_ids = friendships[user_id]
+    return len(friend_ids)
+total_connections = sum(number_of_friends(user) for user in users)
+
+num_friends_by_id = [(user["id"], number_of_friends(user)) for user in users]
+
+num_friends_by_id.sort(key = lambda id_and_friends: id_and_friends[1],reverse=True)
