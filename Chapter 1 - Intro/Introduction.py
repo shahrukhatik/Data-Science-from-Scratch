dfb3bd212542f6af944cdbf69aad55eb52c0e1fd
ShahrukhAtik <shahrukhatik@Gmail.com>
2020-04-01 17:11:10 -0500

    Introduction, Organizing

---
+++ Chapter 1 - Intro/Introduction.py
@@ -0,0 +1,18 @@
+
+
+users = [
+{"id" : 0, "name": "Shahrukh"},
+{"id" : 1, "name": "Mazy"},
+{"id" : 2, "name": "Sartaj"},
+{"id" : 3, "name": "Emily"},
+{"id" : 4, "name": "Sierra"},
+{"id" : 5, "name": "Andy"},
+{"id" : 6, "name": "Rob"},
+{"id" : 7, "name": "Janak"},
+{"id" : 8, "name": "Wasil"},
+{"id" : 9, "name": "Kartik"}
+]
+
+friendship_pairs = [(0,1),(0,2),(1,2),(1,3),(2,3),(3,4),(4,5),(5,6),(5,7),(6,8),(7,8),(8,9)]
+
+friendship_pairs
