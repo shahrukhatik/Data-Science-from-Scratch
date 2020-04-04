62d19c6ba38c4404d3ee31984391305cac9fb889
ShahrukhAtik <shahrukhatik@Gmail.com>
2020-04-04 13:48:55 -0500

    Bar plots

--- Chapter 3/Visualizing Data.py
+++ Chapter 3/Visualizing Data.py
@@ -22,3 +22,19 @@ plt.title("My Favorite Movies")
 plt.ylabel("# of Academy Awards")
 plt.xticks(range(len(movies)),movies)
 plt.show()
+
+###Bar chart - counter
+from collections import Counter
+grades = [83,95,91,87,70,0,85,82,100,67,73,77,0]
+
+histogram = Counter(min(grade // 10*10,90) for grade in grades)
+plt.bar([x+5 for x in histogram.keys()], ## shift bars to right by 5
+        histogram.values(), ### give bars its respective correct height
+        10, ##barwidth of 10
+        edgecolor=(0,0,0)) ## black edges for bar
+plt.axis([-5,105,0,5])
+plt.xticks([10 * i for i in range[11]])
+plt.xlabel("Decile")
+plt.ylabel("# of Students")
+plt.title("Distribution of Exam 1 Grades")
+plt.show
